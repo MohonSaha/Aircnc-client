78dca767d78e3cec02b370808c3998bb59b38d55
MohonSaha <practicewithmohon@gmail.com>
2023-08-23 18:28:53 +0600

    implement calender

--- src/components/Rooms/RoomReservation.jsx
+++ src/components/Rooms/RoomReservation.jsx
@@ -1,9 +1,25 @@
+import Button from "../Button/Button";
+import Calender from "./Calender";
 
 
 const RoomReservation = () => {
     return (
-        <div>
-            
+        <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
+            <div className="flex flex-row items-center gap-1 p-4">
+                <div className="text-2xl font-semibold">$ 200</div>
+                <div className="font-medium text-neutral-600">night</div>
+            </div>
+            <hr />
+            <Calender/>
+            <hr />
+            <div className="p-4">
+                <Button label={"Reserve"}></Button>
+            </div>
+            <hr />
+            <div className="p-4 flex flex-row items-center justify-between font-semibold text-lg">
+                <div>Total</div>
+                <div>$ 200</div>
+            </div>
         </div>
     );
 };
--- src/pages/RoomsDetails/RoomsDetails.jsx
+++ src/pages/RoomsDetails/RoomsDetails.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from '../../components/shared/Container/Container';
 import Header from '../../components/Rooms/Header';
 import RoomInfo from '../../components/Rooms/RoomInfo';
+import RoomReservation from '../../components/Rooms/RoomReservation';
 
 const RoomsDetails = () => {
     return (
@@ -10,8 +11,10 @@ const RoomsDetails = () => {
                 <div className='flex flex-col gap-6'>
                     <div><Header></Header></div>
                     <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
-                        <RoomInfo/>
-                        <div>Reservation</div>
+                        <RoomInfo />
+                        <div className='mb-10 md:col-span-3'>
+                            <RoomReservation />
+                        </div>
                     </div>
                 </div>
             </div>
